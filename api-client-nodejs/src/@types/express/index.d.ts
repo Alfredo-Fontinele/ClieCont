e34e9c97f3db78833b89ba067100b55ddab8b0bc
Alfredo <alfredofontinele08@gmail.com>
2023-03-28 17:30:44 -0300

    deploy : config

--- api-client-nodejs/src/@types/express/index.d.ts
+++ api-client-nodejs/src/@types/express/index.d.ts
@@ -1,10 +1,6 @@
 import { Client } from "./../../entities/Client";
 import { Contact } from "./../../entities/Contact";
 
-declare module "supertest";
-declare module "express";
-declare module "jsonwebtoken";
-
 declare global {
     namespace Express {
         interface Request {
