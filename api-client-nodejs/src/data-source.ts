bcc996b803a9523470dbde2f95b18c6a065acc15
alfredo <alfredofontinele08@gmail.com>
2023-07-18 19:58:55 -0300

    refac: update imports from entities and migrations

--- api-client-nodejs/src/data-source.ts
+++ api-client-nodejs/src/data-source.ts
@@ -1,22 +1,21 @@
-import { DataSource, DataSourceOptions } from "typeorm";
-import path from "path";
-import "reflect-metadata";
 import "dotenv/config";
+import "reflect-metadata";
+import { DataSource, DataSourceOptions } from "typeorm";
+import { Client } from "./entities/Client";
+import { Contact } from "./entities/Contact";
+import { createEntities1679769931928 } from "./migrations/1679769931928-createEntities";
 
 const setDataSourceConfig = (): DataSourceOptions => {
-    const entitiesPath: string = path.join(__dirname, "./entities/**.{js,ts}");
-    const migrationsPath: string = path.join(
-        __dirname,
-        "./migrations/**.{js,ts}"
-    );
+    const entitiesPath = [Client, Contact]
+    const migrationsPath = [createEntities1679769931928]
     const nodeEnv = process.env.NODE_ENV;
 
     if (nodeEnv === "production") {
         return {
             type: "postgres",
             url: process.env.DATABASE_URL,
-            entities: [entitiesPath],
-            migrations: [migrationsPath],
+            entities: entitiesPath,
+            migrations: migrationsPath,
         };
     }
 
@@ -25,7 +24,7 @@ const setDataSourceConfig = (): DataSourceOptions => {
             type: "sqlite",
             database: ":memory:",
             synchronize: true,
-            entities: [entitiesPath],
+            entities: entitiesPath,
         };
     }
 
@@ -38,8 +37,8 @@ const setDataSourceConfig = (): DataSourceOptions => {
         database: process.env.PGDATABASE,
         logging: true,
         synchronize: false,
-        entities: [entitiesPath],
-        migrations: [migrationsPath],
+        entities: entitiesPath,
+        migrations: migrationsPath,
     };
 };
 
